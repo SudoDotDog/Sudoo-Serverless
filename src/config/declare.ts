55a7c66fabaed3022249d360a7642008348e8877
wmxpy <wm@mengw.io>
2022-06-05 12:41:20 -0500

    init config declare

---
+++ src/config/declare.ts
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @namespace Config
+ * @description Declare
+ */
+
+export type ServerlessConfigVersion1 = {
+
+    readonly version: 1;
+};
+
+export type ServerlessConfig =
+    | ServerlessConfigVersion1;
