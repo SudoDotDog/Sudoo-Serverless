3467e6b6c4de3987976226f472077b012766c96b
wmxpy <wm@mengw.io>
2022-06-05 20:58:32 -0500

    improve dry urn enable

--- src/cloud/deploy-aws-lambda.ts
+++ src/cloud/deploy-aws-lambda.ts
@@ -5,12 +5,14 @@
  */
 
 import * as ChildProcess from "child_process";
+import * as Path from "path";
 import { ServerlessConfigFunctionTypeVersion1, ServerlessConfigFunctionVersion1 } from "../config/declare/v1";
 import { ExecuteOption } from "../declare";
+import { logInfo } from "../util/log";
 import { fixOutputPath } from "../util/output-path";
 
 export const deployAWSLambda = (
-    option: ExecuteOption,
+    options: ExecuteOption,
     base: string,
     target: ServerlessConfigFunctionVersion1,
 ): Promise<void> => {
@@ -25,21 +27,34 @@ export const deployAWSLambda = (
     ) => {
 
         const outputPath: string = fixOutputPath(base, target.outputPath);
+        const outputZipPath: string = Path.join(outputPath, 'compressed.zip');
 
-        const awsProcess = ChildProcess.spawn('aws', [
+        const commandArgs = [
             'lambda',
             'update-function-code',
             '--function-name',
             target.functionName,
             '--no-cli-pager',
             '--zip-file',
-            `fileb://${outputPath}`,
-        ]);
+            `fileb:/${outputZipPath}`,
+        ];
+
+        if (options.dryRun) {
+            logInfo(`Dry Run Enabled, Skip Deploy Target ${target.name}`);
+
+            const command = `aws ${commandArgs.join(' ')}`;
+            logInfo(`Command: ${command}`);
+
+            resolve();
+            return;
+        }
+
+        const awsProcess = ChildProcess.spawn('aws', commandArgs);
 
         awsProcess.on('close', (code: number) => {
 
             if (code !== 0) {
-                throw new Error(`Failed to deploy AWS Lambda: ${target.functionName}`);
+                reject(new Error(`Failed to deploy target ${target.name}`));
             }
             resolve();
         });
--- src/recipe/deploy/deploy-target.ts
+++ src/recipe/deploy/deploy-target.ts
@@ -17,10 +17,5 @@ export const deployTarget = async (
 
     logInfo(`Deploy Target: ${target.name}`);
 
-    if (options.dryRun) {
-        logInfo('Dry Run Enabled, Skip Deploy');
-        return;
-    }
-
     await deployAWSLambda(options, base, target);
 };
